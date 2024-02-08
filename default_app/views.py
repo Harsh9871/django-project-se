1ff81c481bca760409783518357b7aaa205dca3f
Harsh Raithatha <158036399+Harsh9871@users.noreply.github.com>
2024-02-08 12:13:17 +0530

    fix gallery

--- default_app/views.py
+++ default_app/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
-# Create your views here.
-
-
+import json
 def index(request):
-    return render(request,'default_app/index.html')
-
+    return render(request, 'default_app/index.html')
 
 def gallery(request):
-    return render(request,'default_app/gallery.html')
+    output = []
+    for i in range(157):
+        qwert = str(i)
+        myvar = qwert
+        output.append(myvar)
+        # output += f'''
+        #     <div class="col-lg-4 col-sm-6 mb-4">
+        #         <div class="portfolio-item">
+        #             <a>
+        #                 <div>
+        #                     <div class="portfolio-hover-content"></div>
+        #                 </div>
+        #                 <div class="img-hover">
+        #                     <img class="img-fluid myimagebody" src="{myvar}" alt="..." style="object-fit: cover;"/>
+        #                 </div>
+        #             </a>
+        #         </div>
+        #     </div>
+        # '''
 
+    return render(request, 'default_app/gallery.html', {"output": output})
 
 def terms(request):
-    return render(request,'default_app/tc.html')
-
+    return render(request, 'default_app/tc.html')
 
 def privacy(request):
-    return render(request,'default_app/pp.html')
+    return render(request, 'default_app/pp.html')
